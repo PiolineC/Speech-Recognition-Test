1217bd3f356cd0009284c785fd66c0c7882dcf6a
Pio <18477395+PiolineC@users.noreply.github.com>
2018-03-17 19:27:16 -0500

    ex's and the oh oh oh's they haunt me

--- script.js
+++ script.js
@@ -8,15 +8,38 @@ $(document).ready(() => {
     recognition.lang = 'en-US';
     recognition.continuous = true;
 
+    let turn = "X";
+
     $("button").click(change);
 
     function change() {
         recognition.start();
     } 
 
+    function parse(str) {
+        let row = 0;
+        let col = 0;
+        switch(str){
+            case 'one': row = 0; col = 0; break;
+            case 'two': row = 0; col = 1; break;
+            case 'three': row = 0; col = 2; break;
+            case 'four': row = 1; col = 0; break;
+            case 'five': row = 1; col = 1; break;
+            case 'six': row = 1; col = 2; break;
+            case 'seven': row = 2; col = 0; break;
+            case 'eight': row = 2; col = 1; break;
+            case 'nine': row = 2; col = 2; break;
+
+            $("table tr:nth-child(" + row + ") td:nth-child(" + col + ")").text(turn);
+        }
+        turn = turn === 'X' ? 'O' : 'X'; 
+        recognition.start();
+    }
+
     recognition.onresult = function(event) {
         const text = event.results[0][0]["transcript"];
         $(".text").text("You said: " + text);
         recognition.stop();
+        parse(text);
     } 
 });
